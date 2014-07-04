2d637571406c611d4197381959c16b85c1224318
Neeme Takis <neeme@takis.ee>
2014-07-04 22:53:36 +0300

    igaks juhuks 7.4.2014

--- getnetwork.sh
+++ getnetwork.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 #techbase variant, bin/sh nmitte /usr/bin/bash
-# parameter ether or smthg
-
-# mac vahetus tehtav /mnt/mtd/syscfg
-
-#tagastab jargmise:
-
-#[root@techbase /mnt/nand-user/d4c]# ./getnetwork.sh
-#1883C404336C 10.0.0.121/24
-
-#neeme 2013
-
+#neeme takis 2013
 if [ "$1" = "" ]; then
     iface="ether"
 else
     iface=$1
 fi
-#ip addr | cut -c5- | grep -A1 $iface | tr -d "\n" | tr -d ":" | tr '[a-f]' '[A-F]' | cut -d" " -f2,5
+#ip addr | cut -c5- | grep -A1 $iface | tr -d "\n" | tr -d ":" | tr '[a-f]' '[A-F]' | cut -d" " -f2,5 # kui mac allub syscfg-le
+#exit 0
 
 # kui ei allu syscfg poolt maaratule
 mac=`cat /mnt/mtd/syscfg | grep HOST_MAC | cut -d"=" -f2 | cut -d" " -f1 | sed 's/://g'`
-ip=`ip addr | cut -c5- | grep -A2 eth0 | grep inet | cut -d" " -f2` #
 
+ipa=`ip addr`
+if [ `echo "$ipa" | grep eth0 | grep DOWN | wc -l` -gt 0 ]; then
+  # echo eth0 down # debug
+  ip=`echo "$ipa" | cut -c5- | grep -A2 tun0 | grep inet | cut -d" " -f2`
+  if [ "$ip" = "" ]; then
+    ip=`echo "$ipa" | cut -c5- | grep -A2 ppp0 | grep peer | cut -d" " -f2`
+  fi
+else
+  ip=`echo "$ipa" | cut -c5- | grep -A2 eth0 | grep inet | cut -d" " -f2`
+fi
 echo $mac $ip
+
+# puudus- kui eth kasutusel, siis vpn ip ei anna!
